3477a4d27aba6277ad27613ef8c4d27794b0bf40
Mac <louis.niuniu@gmail.com>
2022-11-25 10:10:04 +0100

    add mybatis xml files

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.example.mapper;
+
+import com.example.dao.Users;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface UserMapper {
+//    @Select( "select * from Users;" )
+    public List<Users> selectAll();
+}
---
+++ src/main/java/com/example/test.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import com.example.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession( );
+        UserMapper mapper = session.getMapper( UserMapper.class );
+        mapper.selectAll();
+        session.close();
+
+    }
+}
